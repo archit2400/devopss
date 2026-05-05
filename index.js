91e7c8398610231dd5b423f25be4576c5af135b1
Archit <143813492+archit2400@users.noreply.github.com>
2026-05-05 15:05:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello world 🚀')
+})
+
+// 👇 VERY IMPORTANT for Docker
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`)
+})
